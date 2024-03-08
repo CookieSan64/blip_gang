0fbca624afee13f024cf1ea7fb0755c7e0f3b13c
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 03:15:41 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+author 'CookieSan#5805'
+description 'Ajouter des blips de gang'
+version '1.0.0'
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
+
+shared_script '@es_extended/imports.lua'
---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+Config.Map = {
+  --{name="Gang",             color=39,   id=630,   x= 1967.366,    y = 5168.596,     z=47.6219},       --Ghost
+  --{name="Gang",             color=39,   id=630,   x= 70.35802,    y = 3639.777,     z=40.83813},      --The Lost MC
+  --{name="Gang",             color=39,   id=630,   x= 520.7762,    y = -2765.677,    z=29.44124},      --Pariah
+  --{name="Gang",             color=39,   id=630,   x= -1574.9,     y = -264.7719,    z=51.19353},      --Miami Boy
+  --{name="Gang",             color=39,   id=630,   x= -180.2002,   y = -1597.347,    z=40.22742},      --Families
+  --{name="Gang",             color=39,   id=630,   x= 5.388107,    y = -1831.247,    z=27.90624},      --Ballas
+  --{name="Gang",             color=39,   id=630,   x= 328.3514,    y = -2036.344,    z=25.24176},      --Vagos
+  --{name="Gang",             color=39,   id=630,   x= 483.8749,    y = -1523.91,     z=36.91382},      --Aztecaz
+  --{name="Gang",             color=39,   id=630,   x= 1255.936,    y = -1569.011,    z=58.3517},       --Marabunta
+  --{name="Gang",             color=39,   id=630,   x= -638.2344,   y = -1246.3,      z=11.81509},      --Triade
+  --{name="Gang",             color=39,   id=630,   x= 2228.931,    y = 5633.427,     z=55.99633},      --Ferme a Weed
+  --{name="Gang",             color=39,   id=630,   x= 1444.882,    y = 6355.26,      z=25.92719},      --Communiste
+  --{name="Gang",             color=39,   id=630,   x= -1114.708,   y = 4924.942,     z=219.9335},      --Altruiste
+
+  {name="Cartel",             color=49,   id=630,   x= 1369.5,      y = 1149.80,      z= 20.74},        --Villa La Fuente Blanca
+  {name="Cartel",             color=49,   id=630,   x= 4997.7,      y = -5741.92,     z= 26.12},        --Manoir Cayo Perico
+
+}
